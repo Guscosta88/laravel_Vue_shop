85a1c0d2157042e864cf28b26dbaba3b02c2c808
Guscosta88 <guslfcosta@gmail.com>
2022-05-26 12:30:21 +1000

    add-to-cart-button functionality added

--- resources/js/components/AddToCart.vue
+++ resources/js/components/AddToCart.vue
@@ -1,12 +1,27 @@
 <template>
     <div>
         <hr>
-         <button class="btn btn-warning text-center">Add To Cart</button>
+         <button class="btn btn-warning text-center" 
+                v-on:click.prevent="addProductToCart()">
+                Add To Cart
+        </button>
     </div>
 </template>
 
 <script>
     export default {
+        data(){
+
+        },
+        props:['productId', 'userId'],
+        methods:{
+            addProductToCart(){
+                if(this.userId == 0){
+                    this.$toastr.e('You Need to login, To add this product in the Cart');
+                    return;
+                }
+            }
+        },
         mounted() {
             console.log('Component mounted.')
         }
